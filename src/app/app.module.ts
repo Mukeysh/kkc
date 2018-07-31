122a8d61f15d293ccb5caf550afaceb5c038c76a
Mukesh <mukeysh@gai.co.in>
2018-07-31 14:25:53 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { StepsComponent } from './steps/steps.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    StepsComponent
   ],
   imports: [
     BrowserModule
